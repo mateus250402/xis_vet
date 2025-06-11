f1c19913cf8e0c79bfb8f262cba5102114b0a3c6
mateus250402 <cardoso.olim@gmail.com>
2025-06-11 19:12:50 -0300

    senha

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
         'host': 'localhost',
         'port': '3306',
         'user': 'root',
-        'password': '021086',
+        'password': '021086Aa!',
         'database': 'vendas'
     }
 
